2b071afec4eb082589e368a2a1795ff67fff79e2
muhammad-jiku <muhammadjiku364@gmail.com>
2023-03-19 22:25:04 +0600

    user status change endpoint

--- src/components/Dashboard/Admin/Users/UserTableRow.jsx
+++ src/components/Dashboard/Admin/Users/UserTableRow.jsx
@@ -28,15 +28,47 @@ const UserTableRow = ({ user, idx, refetch }) => {
       })
       .catch((err) => console.log(err));
   };
+
+  const removeAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: 'PATCH',
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem('accessToken')}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error('Failed to remove as an admin');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data?.modifiedCount > 0) {
+          refetch();
+          toast.success(`${email} successfully excluded as admin`);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <tr>
       <th>{idx + 1}</th>
       <td>{email}</td>
       <td>
         {role === 'admin' ? (
-          <button className="btn font-bold uppercase text-white" disabled>
-            Admin
-          </button>
+          <>
+            <button className="btn font-bold uppercase text-white" disabled>
+              Admin
+            </button>
+            <button
+              className="btn font-bold uppercase text-white ml-2"
+              onClick={removeAdmin}
+            >
+              Remove Admin
+            </button>
+          </>
         ) : (
           <button
             className="btn font-bold uppercase text-white"
@@ -45,9 +77,6 @@ const UserTableRow = ({ user, idx, refetch }) => {
             Make Admin
           </button>
         )}
-        <button className="btn font-bold uppercase text-white ml-2">
-          Remove Admin
-        </button>
       </td>
       {/* <td>Blue</td> */}
     </tr>
